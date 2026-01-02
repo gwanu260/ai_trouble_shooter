efa63f75e28ae6cc8ffbc1a31637825c9c9bf580
Kwanwoo KIM <gwnau260@naver.com>
2026-01-02 12:36:13 +0900

    Feat: test 38

--- main/llm/agent_with_graph.py
+++ main/llm/agent_with_graph.py
@@ -5,8 +5,8 @@ from langgraph.graph import StateGraph, START, END, MessagesState
 from langgraph.prebuilt import ToolNode, tools_condition
 from langchain_core.messages import SystemMessage, HumanMessage, ToolMessage
 from langchain_anthropic import ChatAnthropic
-from dev.app.llm.prompts import PROMPTS
-from dev.app.llm.tools import rag_search_tool
+from main.llm.prompts import PROMPTS
+from main.llm.tools import rag_search_tool
 # from prompts import PROMPTS
 # from tools import rag_search_tool
 load_dotenv()
